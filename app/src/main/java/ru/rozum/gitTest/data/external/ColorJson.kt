af192bb93d7f25d0bafb668b63e85e94415b1fba
Alexander Rozum <roozumaleks@gmail.com>
2024-03-28 18:57:54 +0300

    Рефакторинг ColorJson.kt

--- app/src/main/java/ru/rozum/gitTest/data/external/ColorJson.kt
+++ app/src/main/java/ru/rozum/gitTest/data/external/ColorJson.kt
@@ -1,9 +1,6 @@
 package ru.rozum.gitTest.data.external
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ColorJson(
-    @SerialName("color") val color: String
-)
+data class ColorJson(val color: String)
