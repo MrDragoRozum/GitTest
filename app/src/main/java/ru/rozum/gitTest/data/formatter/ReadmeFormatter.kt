8dd291e045f5753bb8af8968702ad4492b263d46
Alexander Rozum <roozumaleks@gmail.com>
2024-07-19 18:45:42 +0300

    Создан ReadmeFormatter.kt и был перемещён всё это в этот файл. Также попытка ренейминга в AppRepositoryImpl.kt

---
+++ app/src/main/java/ru/rozum/gitTest/data/formatter/ReadmeFormatter.kt
@@ -0,0 +1,51 @@
+package ru.rozum.gitTest.data.formatter
+
+fun StringBuilder.getFormattedReadmeWithLinksImage(
+    ownerName: String,
+    repositoryName: String,
+    branchName: String
+): String {
+    /*
+    * Ищет в README.md подобные подстроки: images/screenshot_1.png screenshots/image1.jpg и т.п.
+    * дабы получилось в итоге https://raw.githubusercontent.com/owner/project/branch/images/screenshot_1.png
+    * в противном случае Markwon не сможет загрузить графический файл
+    */
+    val regexParsingImagesFromFolderRepo =
+        Regex("(?<=!\\[alt text]\\(|\\[logo]:)(\\s)*(\\w+)/(\\w+)[.](\\w{2,3})(?=[)]?)")
+
+    regexParsingImagesFromFolderRepo
+        .findAll(this)
+        .map { it.value }
+        .forEach { foundLinkImage ->
+            addURLForLinkImage(
+                this,
+                foundLinkImage,
+                ownerName,
+                repositoryName,
+                branchName
+            )
+        }
+
+    return this.toString()
+}
+
+private fun addURLForLinkImage(
+    builder: StringBuilder,
+    foundLinkImage: String,
+    ownerName: String,
+    repositoryName: String,
+    branchName: String
+) {
+    builder.replace(
+        Regex(foundLinkImage), URI_RAW_GITHUB_WITH_FORMATTERS.format(
+            ownerName,
+            repositoryName,
+            branchName,
+            foundLinkImage
+        )
+    ).also { result ->
+        builder.setRange(0, result.length, result)
+    }
+}
+
+private const val URI_RAW_GITHUB_WITH_FORMATTERS = "https://raw.githubusercontent.com/%s/%s/%s/%s"
--- app/src/main/java/ru/rozum/gitTest/data/repository/AppRepositoryImpl.kt
+++ app/src/main/java/ru/rozum/gitTest/data/repository/AppRepositoryImpl.kt
@@ -4,24 +4,17 @@ import android.content.Context
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.decodeFromJsonElement
-import kotlinx.serialization.json.decodeFromStream
 import retrofit2.Response
 import ru.rozum.gitTest.R
-import ru.rozum.gitTest.data.external.ColorJson
 import ru.rozum.gitTest.data.local.KeyValueStorage
+import ru.rozum.gitTest.data.formatter.getFormattedReadmeWithLinksImage
 import ru.rozum.gitTest.data.mapper.toEntity
 import ru.rozum.gitTest.data.network.api.ApiGitHubService
-import ru.rozum.gitTest.data.network.dto.RepoDto
 import ru.rozum.gitTest.data.repository.Level.CODE
 import ru.rozum.gitTest.data.repository.Level.MESSAGE
 import ru.rozum.gitTest.data.repository.Level.MESSAGE_CODE
 import ru.rozum.gitTest.data.repository.entity.LevelException
 import ru.rozum.gitTest.di.RegexLegalTokenQualifier
-import ru.rozum.gitTest.di.RegexParsingImagesFromFolderQualifier
 import ru.rozum.gitTest.domain.entity.Repo
 import ru.rozum.gitTest.domain.entity.RepoDetails
 import ru.rozum.gitTest.domain.entity.UserInfo
@@ -34,13 +27,12 @@ class AppRepositoryImpl @Inject constructor(
     private val client: KeyValueStorage,
     private val dispatcherIO: CoroutineDispatcher,
     @RegexLegalTokenQualifier private val regexLegalToken: Regex,
-    @RegexParsingImagesFromFolderQualifier private val regexParsingImagesFromFolder: Regex,
     @ApplicationContext val context: Context
 ) : AppRepository {
 
     override fun getToken(): String = client.getToken()
 
-    override suspend fun getRepositories(): List<Repo> = connect(
+    override suspend fun getRepositories(): List<Repo> = executeRequest(
         response = withContext(dispatcherIO) {
             apiGitHubService.getRepositories()
         },
@@ -75,7 +67,7 @@ class AppRepositoryImpl @Inject constructor(
                 throw IllegalArgumentException(context.getString(R.string.invalid_token))
         }
 
-        return connect(
+        return executeRequest(
             response = withContext(dispatcherIO) {
                 apiGitHubService.signIn(legalToken)
             },
@@ -93,49 +85,33 @@ class AppRepositoryImpl @Inject constructor(
         ownerName: String,
         repositoryName: String,
         branchName: String
-    ): String = connect(
+    ): String = executeRequest(
         response = apiGitHubService.getRepositoryReadme(ownerName, repositoryName, branchName),
         levelException = LevelException(
             context.getString(R.string.connection_error),
             CODE
         )
     ) { readme ->
-        val builder = StringBuilder(readme)
-        regexParsingImagesFromFolder
-            .findAll(builder)
-            .map { it.value }
-            .forEach { foundLinkImage ->
-                addURLForLinkImage(builder, foundLinkImage, ownerName, repositoryName, branchName)
-            }
-        builder.toString()
+        StringBuilder(readme).getFormattedReadmeWithLinksImage(
+            ownerName,
+            repositoryName,
+            branchName
+        )
     }
 
-    private fun addURLForLinkImage(
-        builder: StringBuilder,
-        foundLinkImage: String,
-        ownerName: String,
-        repositoryName: String,
-        branchName: String
-    ) {
-        builder.replace(
-            Regex(foundLinkImage), URI_RAW_GITHUB_WITH_FORMATTERS.format(
-                ownerName,
-                repositoryName,
-                branchName,
-                foundLinkImage
-            )
-        ).also { result ->
-            builder.setRange(0, result.length, result)
-        }
-    }
-
-    private inline fun <T, V> connect(
+    private inline fun <T, V> executeRequest(
         response: Response<T>,
         levelException: LevelException,
         result: (T) -> V
     ): V {
         if (response.isSuccessful) return result(response.body()!!)
+        returnNetworkError(response, levelException)
+    }
 
+    private fun <T> returnNetworkError(
+        response: Response<T>,
+        levelException: LevelException
+    ): Nothing {
         val code = response.code()
         val message = when (levelException.level) {
             MESSAGE -> levelException.message
@@ -147,10 +123,6 @@ class AppRepositoryImpl @Inject constructor(
             )
         }
         throw ConnectException(message)
-    }
-
-    private companion object {
-        const val URI_RAW_GITHUB_WITH_FORMATTERS = "https://raw.githubusercontent.com/%s/%s/%s/%s"
     }
 }
 
