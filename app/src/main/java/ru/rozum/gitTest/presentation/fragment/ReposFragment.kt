64267ea1763e2af5bc38131059606f819b01018c
Alexander Rozum <roozumaleks@gmail.com>
2024-07-24 14:12:56 +0300

    Рефакторинг ReposFragment.kt

--- app/src/main/java/ru/rozum/gitTest/presentation/fragment/ReposFragment.kt
+++ app/src/main/java/ru/rozum/gitTest/presentation/fragment/ReposFragment.kt
@@ -1,58 +1,42 @@
 package ru.rozum.gitTest.presentation.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
-import ru.rozum.gitTest.databinding.FragmentRepositoriesListBinding
+import ru.rozum.gitTest.R
+import ru.rozum.gitTest.databinding.FragmentReposBinding
 import ru.rozum.gitTest.presentation.fragment.adapter.RepositoryListAdapter
-import ru.rozum.gitTest.presentation.fragment.util.*
+import ru.rozum.gitTest.presentation.fragment.util.collect
+import ru.rozum.gitTest.presentation.fragment.util.setVisibility
 import ru.rozum.gitTest.presentation.fragment.viewModel.RepositoriesListViewModel
 import ru.rozum.gitTest.presentation.fragment.viewModel.RepositoriesListViewModel.State
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class RepositoriesListFragment : Fragment() {
-
-    private var _binding: FragmentRepositoriesListBinding? = null
-    private val binding get() = _binding!!
+class ReposFragment : Fragment(R.layout.fragment_repos) {
+    private val binding by viewBinding(FragmentReposBinding::bind)
+    private val viewModel by viewModels<RepositoriesListViewModel>()
+    private val args by navArgs<ReposFragmentArgs>()
 
     @Inject
     lateinit var adapter: RepositoryListAdapter
 
-    private val viewModel by viewModels<RepositoriesListViewModel>()
-
-    private val args by navArgs<RepositoriesListFragmentArgs>()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentRepositoriesListBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        install()
+        installFragment()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    private fun install() {
+    private fun installFragment() {
         installAdapter()
-        state()
-        listeners()
+        installState()
+        installListeners()
     }
 
     private fun installAdapter() {
@@ -62,26 +46,35 @@ class RepositoriesListFragment : Fragment() {
         )
     }
 
-    private fun state() {
-        collect(viewLifecycleOwner, viewModel.state) {
-            installState(it)
+    private fun installState() {
+        collect(viewLifecycleOwner, viewModel.state) { state ->
+            binding.progressBarRepoDetails.visibility = setVisibility(
+                state,
+                State.Loading
+            )
+            binding.includeEmptyRepoList.root.visibility = setVisibility(
+                state,
+                State.Empty
+            )
+            binding.includeSomethingErrorRepoList.root.visibility =
+                setVisibility(
+                    state,
+                    State.SomethingError
+                )
+            binding.includeConnectionErrorRepoList.root.visibility = setVisibility(
+                state,
+                State.ConnectionError
+            )
+
+            changeStateReposIfListLoaded(state)
         }
     }
 
-    private fun installState(state: State) {
-        binding.progressBarRepoDetails.visibility = setVisibility(state, State.Loading)
-        binding.includeEmptyRepoList.root.visibility = setVisibility(state, State.Empty)
-
-        binding.includeSomethingErrorRepoList.root.visibility =
-            setVisibility(state, State.SomethingError)
-
-        binding.includeConnectionErrorRepoList.root.visibility =
-            setVisibility(state, State.ConnectionError)
-
+    private fun changeStateReposIfListLoaded(state: State) {
         if (state is State.Loaded) adapter.submitList(state.repos)
     }
 
-    private fun listeners() {
+    private fun installListeners() {
         listOf(
             binding.includeEmptyRepoList.buttonRefreshEmpty,
             binding.includeSomethingErrorRepoList.buttonRetrySomethingError,
@@ -93,8 +86,7 @@ class RepositoriesListFragment : Fragment() {
         }
         adapter.onClickRepo = { repo ->
             findNavController().navigate(
-                RepositoriesListFragmentDirections
-                    .actionRepositoriesListFragmentToDetailInfoFragment(args.userInfo, repo)
+                ReposFragmentDirections.actionReposFragmentToRepoFragment(args.user, repo)
             )
         }
     }
